f5cfbbb244a80e46cb20410f47d646f2de4cae4c
Alex <j201.alex@gmail.com>
2017-08-29 01:10:34 -0400

    S/tile/block

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,7 +14,7 @@ import qualified Data.MultiSet as MS
 import Control.Monad
 import Data.IORef
 
-data Tile = Dirt | Stone | Air
+data Block = Dirt | Stone | Air
     deriving (Eq, Show, Ord)
 
 data InputMode = Normal | Remove | Place
@@ -22,18 +22,18 @@ data InputMode = Normal | Remove | Place
 
 data Game = Game {
     _loc :: V3 Int,
-    _inventory :: MultiSet Tile,
-    _selected :: Maybe Tile,
-    _world :: Array (V3 Int) Tile,
+    _inventory :: MultiSet Block,
+    _selected :: Maybe Block,
+    _world :: Array (V3 Int) Block,
     _inputMode :: InputMode,
-    _worldChanges :: [(V3 Int, Tile)]
+    _worldChanges :: [(V3 Int, Block)]
 }
 
 makeLenses ''Game
 
 maxDim = 10
 
-initWorld :: Array (V3 Int) Tile
+initWorld :: Array (V3 Int) Block
 initWorld = array (negate (V3 maxDim maxDim maxDim), V3 maxDim maxDim maxDim)
                   [(V3 x y z, if z >= 0 then Air
                               else if even (x + y) then Dirt
@@ -63,22 +63,22 @@ keyDir 'd' = V3 0 1 0
 keyDir 'q' = V3 0 0 (-1)
 keyDir 'e' = V3 0 0 1
 
-removeTile :: V3 Int -> Game -> Game
-removeTile dir g = let xyz = g^.loc + dir
-                       t = (g^.world) ! xyz
-                   in if t /= Air
-                      then over worldChanges ((xyz,Air):) $
-                           over inventory (MS.insert t) g
-                      else g
+removeBlock :: V3 Int -> Game -> Game
+removeBlock dir g = let xyz = g^.loc + dir
+                        t = (g^.world) ! xyz
+                    in if t /= Air
+                       then over worldChanges ((xyz,Air):) $
+                            over inventory (MS.insert t) g
+                       else g
 
-placeTile :: V3 Int -> Game -> Game
-placeTile dir g = let xyz = g^.loc + dir
-                  in case g^.selected of
-                      (Just t) -> if ((g^.world) ! xyz) == Air && MS.member t (g^.inventory)
-                                  then over worldChanges ((xyz,t):) $
-                                      over inventory (MS.delete t) g
-                                  else g
-                      _ -> g
+placeBlock :: V3 Int -> Game -> Game
+placeBlock dir g = let xyz = g^.loc + dir
+                   in case g^.selected of
+                       (Just t) -> if ((g^.world) ! xyz) == Air && MS.member t (g^.inventory)
+                                   then over worldChanges ((xyz,t):) $
+                                       over inventory (MS.delete t) g
+                                   else g
+                       _ -> g
 
 commitChanges :: Game -> Game
 commitChanges g = set worldChanges [] $
@@ -95,17 +95,17 @@ updateGame (EvKey (KChar c) mods) g =
                        else g
             _ -> g
         Remove -> if mods == [] && isDirKey c
-                  then removeTile (keyDir c) (set inputMode Normal g)
+                  then removeBlock (keyDir c) (set inputMode Normal g)
                   else (set inputMode Normal g)
         Place -> if mods == [] && isDirKey c
-                 then placeTile (keyDir c) (set inputMode Normal g)
+                 then placeBlock (keyDir c) (set inputMode Normal g)
                  else (set inputMode Normal g)
 updateGame _ g = g
 
-tileImage :: Tile -> Image
-tileImage Dirt = char (defAttr `withForeColor` red) '#'
-tileImage Stone = char (defAttr `withForeColor` blue) '#'
-tileImage Air = char defAttr ' '
+blockImage :: Block -> Image
+blockImage Dirt = char (defAttr `withForeColor` red) '#'
+blockImage Stone = char (defAttr `withForeColor` blue) '#'
+blockImage Air = char defAttr ' '
 
 playerImage = char (defAttr `withForeColor` white) '@'
 
@@ -114,13 +114,13 @@ gameImage g xyz = if xyz == g^.loc
                   then playerImage
                   else let z = xyz^._z
                            w = g^.world
-                           tile = w ! xyz
-                       in if tile == Air && z > (-maxDim)
-                          then tileImage (w ! (xyz + (V3 0 0 (-1))))
-                          else tileImage tile
+                           block = w ! xyz
+                       in if block == Air && z > (-maxDim)
+                          then blockImage (w ! (xyz + (V3 0 0 (-1))))
+                          else blockImage block
 
 status :: Game -> Image
-status g = string defAttr (show (g^.loc)) <-> string defAttr (show (MS.elems (g^.inventory)))
+status g = string defAttr (show (g^.loc)) <-> string defAttr (show (MS.toOccurList (g^.inventory)))
 
 ui :: Game -> Picture
 ui g = let z = view (loc . _z) g
