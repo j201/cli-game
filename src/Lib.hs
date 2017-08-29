5752a7cb6eb0d5025587bf2cbe37e920d90b323a
Alex <j201.alex@gmail.com>
2017-08-29 12:46:59 -0400

    Add inventory scrolling
    And actually add block placement

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,8 +8,8 @@ import Data.Array
 import Linear.V3
 import Lens.Micro.Platform
 import Graphics.Vty
-import Data.MultiSet (MultiSet)
-import qualified Data.MultiSet as MS
+import Data.Sequence (Seq, (|>), (<|), (><))
+import qualified Data.Sequence as DS
 
 import Control.Monad
 import Data.IORef
@@ -22,8 +22,8 @@ data InputMode = Normal | Remove | Place
 
 data Game = Game {
     _loc :: V3 Int,
-    _inventory :: MultiSet Block,
-    _selected :: Maybe Block,
+    _inventory :: Seq (Block,Int),
+    _selected :: Maybe Int,
     _world :: Array (V3 Int) Block,
     _inputMode :: InputMode,
     _worldChanges :: [(V3 Int, Block)]
@@ -45,7 +45,7 @@ initWorld = array (negate (V3 maxDim maxDim maxDim), V3 maxDim maxDim maxDim)
 initGame :: Game
 initGame = Game {
     _loc = (V3 0 0 0),
-    _inventory = MS.empty,
+    _inventory = DS.empty,
     _selected = Nothing,
     _world = initWorld,
     _inputMode = Normal,
@@ -68,20 +68,52 @@ inBounds a xyz = let (lb,ub) = bounds a
                  in all (>= 0) (xyz - lb) &&
                     all (<= 0) (xyz - ub)
 
+selectedItem :: Game -> Maybe Block
+selectedItem g = fmap (\i -> fst $ DS.index (g^.inventory) i) (g^.selected)
+
+addItem :: Block -> Game -> Game
+addItem b = over inventory (\s -> case DS.findIndexL ((== b) . fst) s of
+                                    Just i -> DS.adjust (\(x,y) -> (x,y+1)) i s
+                                    Nothing -> s |> (b,1)) .
+            over selected (\m -> if m == Nothing then Just 0 else m)
+
+-- Not added until containers-0.5.8.0, which isn't in Stackage
+deleteAt :: Int -> Seq a -> Seq a
+deleteAt i s = DS.take i s >< DS.drop (i+1) s
+
+removeItemAt :: Int -> Game -> Game
+removeItemAt i g = let g' = over inventory (\s -> if snd (DS.index s i) == 1
+                                                  then deleteAt i s
+                                                  else DS.adjust (\(x,y) -> (x,y-1)) i s)
+                                 g
+                   in over selected (\m -> case m of
+                                           Just i -> if DS.null (g'^.inventory)
+                                                         then Nothing
+                                                     else if i >= DS.length (g'^.inventory)
+                                                         then Just (i-1)
+                                                     else m
+                                           _ -> m)
+                           g'
+
+removeItem :: Block -> Game -> Game
+removeItem b g = case DS.findIndexL ((== b) . fst) (g^.inventory) of
+                   Just i -> removeItemAt i g
+                   Nothing -> error "Tried to remove item not in inventory!"
+
 removeBlock :: V3 Int -> Game -> Game
 removeBlock dir g = let xyz = g^.loc + dir
-                        t = (g^.world) ! xyz
-                    in if t /= Air
+                        b = (g^.world) ! xyz
+                    in if b /= Air
                        then over worldChanges ((xyz,Air):) $
-                            over inventory (MS.insert t) g
+                            addItem b g
                        else g
 
 placeBlock :: V3 Int -> Game -> Game
 placeBlock dir g = let xyz = g^.loc + dir
                    in case g^.selected of
-                       (Just t) -> if ((g^.world) ! xyz) == Air && MS.member t (g^.inventory)
-                                   then over worldChanges ((xyz,t):) $
-                                       over inventory (MS.delete t) g
+                       (Just i) -> if ((g^.world) ! xyz) == Air
+                                   then over worldChanges ((xyz, fst $ DS.index (g^.inventory) i):) $
+                                        removeItemAt i g
                                    else g
                        _ -> g
 
@@ -94,20 +126,20 @@ commitChanges g = set worldChanges [] $
 
 updateGame :: Event -> Game -> Game
 updateGame (EvKey (KChar c) mods) g =
-    commitChanges $ case g^.inputMode of
-        Normal -> case (c,mods) of
-            ('r',[]) -> set inputMode Remove g
-            ('p',[]) -> set inputMode Place g
-            (c, []) -> if isDirKey c && isValidDir g (keyDir c)
-                       then over loc (+ keyDir c) g
-                       else g
-            _ -> g
-        Remove -> if mods == [] && isDirKey c && isValidDir g (keyDir c)
-                  then removeBlock (keyDir c) (set inputMode Normal g)
-                  else (set inputMode Normal g)
-        Place -> if mods == [] && isDirKey c && isValidDir g (keyDir c)
-                 then placeBlock (keyDir c) (set inputMode Normal g)
-                 else (set inputMode Normal g)
+    commitChanges $ case (g^.inputMode, c, mods) of
+        (_,';',[]) -> over selected (fmap (\x -> (x-1) `mod` (DS.length (g^.inventory)))) g
+        (_,'\'',[]) -> over selected (fmap (\x -> (x+1) `mod` (DS.length (g^.inventory)))) g
+        (Normal,'r',[]) -> set inputMode Remove g
+        (Normal,'p',[]) -> set inputMode Place g
+        (Normal,_,[]) -> if isDirKey c && isValidDir g (keyDir c)
+                         then over loc (+ keyDir c) g
+                         else g
+        (Remove,_,[]) -> if mods == [] && isDirKey c && isValidDir g (keyDir c)
+                         then removeBlock (keyDir c) (set inputMode Normal g)
+                         else (set inputMode Normal g)
+        (Place,_,[]) -> if mods == [] && isDirKey c && isValidDir g (keyDir c)
+                        then placeBlock (keyDir c) (set inputMode Normal g)
+                        else (set inputMode Normal g)
 updateGame _ g = g
 
 blockImage :: Block -> Image
@@ -128,7 +160,9 @@ gameImage g xyz = if xyz == g^.loc
                           else blockImage block
 
 status :: Game -> Image
-status g = string defAttr (show (g^.loc)) <-> string defAttr (show (MS.toOccurList (g^.inventory)))
+status g = string defAttr (show (g^.loc)) <->
+           string defAttr (show (g^.inventory)) <->
+           string defAttr (show (g^.selected))
 
 ui :: Game -> Picture
 ui g = let z = view (loc . _z) g
