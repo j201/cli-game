671aac77f16e5f8e6795088becd1ce63bee11d0d
Alex <j201.alex@gmail.com>
2017-09-09 21:28:04 -0400

    Try forcing 256 color mode

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,6 +14,7 @@ import qualified Data.Sequence as DS
 import Control.Monad.Random (evalRand, getStdGen, mkStdGen)
 import Data.Colour.RGBSpace
 import Data.Colour.RGBSpace.HSL
+import System.Environment (setEnv)
 
 import Debug.Trace
 
@@ -169,6 +170,7 @@ initUIState = do g <- getStdGen
                           }
 
 run = do
+    setEnv "TERM" "xterm-256color"
     cfg <- standardIOConfig
     vty <- mkVty cfg
     ui <- initUIState
